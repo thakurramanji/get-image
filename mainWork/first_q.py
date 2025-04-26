131e1b67fb8378f952b05e496e1e552d506b3a8a
ramanji <thakurramanji@gmail.com>
2025-04-26 22:20:05 +0530

    first done

---
+++ mainWork/first_q.py
@@ -0,0 +1,34 @@
+import os
+
+import requests
+
+base_url = 'https://picsum.photos/'
+
+
+end_point = base_url + 'v2/list'
+
+response = requests.get(end_point)
+
+data = response.json()
+download_urls = []
+ids = []
+
+for d in data:
+    t = {}
+
+    t["download_url"] = d["download_url"]
+    t["id"] = d['id']
+    download_urls.append(t)
+
+os.makedirs("download_images")
+
+for url in download_urls[:10]:
+    image_id = url['id']
+    img_url = url["download_url"]
+
+    image_data = requests.get(img_url)
+
+    with open(f'download_images/{image_id}.jpg', 'wb') as f:
+        f.write(image_data.content)
+
+    print(f'Downloaded {image_id}.jpg')
